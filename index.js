fe31523c52d67498be7e6605b5136a38a65755e0
BaumTheFeljoy <baumthefeljoy@live.de>
2019-11-09 18:36:07 +0100

    added user and auth routes

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const homepage = require("./routes/home")
 const customers = require("./routes/customers")
 const movies = require("./routes/movies")
 const rentals = require("./routes/rentals")
+const users = require("./routes/users")
+const auth = require("./routes/auth")
 const logger = require("./middleware/logger")
 
 const app = express()
@@ -41,8 +43,14 @@ app.use("/", homepage)
 app.use("/api/customers", customers)
 app.use("/api/movies", movies)
 app.use("/api/rentals", rentals)
+app.use("/api/users", users)
+app.use("/api/auth", auth)
 
 // Configuration details
+if(!config.get("jwtPrivateKey")) {
+    console.error("FATAL ERROR: jwtPrivateKey is not defined")
+    process.exit(1)
+} 
 console.log("Application name: " + config.get("name"))
 console.log("Application mail server: " + config.get("mail.host"))
 console.log("Application mail password: " + config.get("mail.password")) // Windows: $env:app_password = "anything" 
---
+++ models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose")
+const Joi = require("joi")
+const config = require("config")
+const jwt = require("jsonwebtoken")
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 255,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 255
+    },
+    password: {
+        type: String,
+        minlength: 8,
+        maxlength: 1024,
+        required: true
+    }
+})
+
+userSchema.methods.generateAuthToken = function() { // Arrow function syntax doesn't work with "this" for self reference
+    return jwt.sign({ _id: this._id }, config.get("jwtPrivateKey")) // $env:vidly_jwtPrivateKey = "anything"
+}
+
+const User = mongoose.model("User", userSchema)
+
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(1).max(255).required(),
+        email: Joi.string().min(1).max(255).required().email(),
+        password: Joi.string().max(255).min(8).required()
+    }
+
+    return Joi.validate(user, schema)
+}
+
+exports.validate = validateUser
+exports.User = User
